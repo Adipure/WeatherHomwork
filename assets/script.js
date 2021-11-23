e26b61b5ac9f00a2a3644251d20dc5cf4466df6b
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2021-11-22 23:40:52 -0800

    fixed error

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ const APIkey ="ae38efa85aa51a29d76f31334b912676"
 
 document.getElementById("search-btn").addEventListener("click", event => {
   event.preventDefault()
+  document.getElementById('today').innerHTML=""
+  document.getElementById('future').innerHTML=""
   const cityName = document.getElementById("location").value
 
   axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=ae38efa85aa51a29d76f31334b912676`)
@@ -24,7 +26,7 @@ document.getElementById("search-btn").addEventListener("click", event => {
      date: weather.list[0].dt_txt,
      temp: weather.list[0].main.temp,
      humid: weather.list[0].main.humidity,
-     windS: weather.list[0].wind.speed,
+     winds: weather.list[0].wind.speed,
      icon: weather.list[0].weather[0].icon,
      uvi : uvi
     }
@@ -32,7 +34,7 @@ document.getElementById("search-btn").addEventListener("click", event => {
      date: weather.list[8].dt_txt,
      temp: weather.list[8].main.temp,
      humid: weather.list[8].main.humidity,
-     windS: weather.list[8].wind.speed,
+     winds: weather.list[8].wind.speed,
      icon: weather.list[8].weather[0].icon
     }
 
@@ -40,28 +42,28 @@ document.getElementById("search-btn").addEventListener("click", event => {
      date: weather.list[16].dt_txt,
      temp: weather.list[16].main.temp,
      humid: weather.list[16].main.humidity,
-     windS: weather.list[16].wind.speed,
+     winds: weather.list[16].wind.speed,
      icon: weather.list[16].weather[0].icon
     }
     let day3 = {
      date: weather.list[24].dt_txt,
      temp: weather.list[24].main.temp,
      humid: weather.list[24].main.humidity,
-     windS: weather.list[24].wind.speed,
+     winds: weather.list[24].wind.speed,
      icon: weather.list[24].weather[0].icon
     }
     let day4 = {
      date: weather.list[32].dt_txt,
      temp: weather.list[32].main.temp,
      humid: weather.list[32].main.humidity,
-     windS: weather.list[32].wind.speed,
+     winds: weather.list[32].wind.speed,
      icon: weather.list[32].weather[0].icon
     }
     let day5 = {
      date: weather.list[39].dt_txt,
      temp: weather.list[39].main.temp,
      humid: weather.list[39].main.humidity,
-     windS: weather.list[39].wind.speed,
+     winds: weather.list[39].wind.speed,
      icon: weather.list[39].weather[0].icon
     }
     console.log(res.data);
@@ -73,12 +75,12 @@ document.getElementById("search-btn").addEventListener("click", event => {
             <h3>City: ${city} <img src="http://openweathermap.org/img/w/${current.icon}.png" alt="icon"></h3>
             <h3>Temperature: ${current.temp}</h3>
             <h3>Humidity: ${current.humid}</h3>
-            <h3>Wind: ${current.wind}</h3>
+            <h3>Winds: ${current.winds}</h3>
             <h3>UVI: ${uvi}</h3>
           </div>
     `
 
-    documet.getElementById('today').append(currentElm)
+    document.getElementById('today').append(currentElm)
 
 
     let forecasts = [day1, day2, day3, day4, day5]
@@ -95,12 +97,13 @@ document.getElementById("search-btn").addEventListener("click", event => {
             <p class="card-text">            
             <h3>temp: ${day.temp}</h3>
             <h3>humid: ${day.humid}<h3>
-            <h3>wind: ${day.wind}</h3></p>
+            <h3>wind: ${day.winds}</h3></p>
             </div>
             </div>
             </div>
         `
        document.getElementById('future').append(forecastElem)
+      
      })
  })
 })
